4dc6da004371c65e81cc009ad590020efbf315e3
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2019-12-05 20:03:11 -0800

    notetaker2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var path = require("path");
-var server = http.createServer(handleRequest);
+// var server = http.createServer(handleRequest);
 
 // Sets up the Express App
 var app = express();
-var PORT = 8585;
+var PORT = process.env.PORT || 8585;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -44,7 +44,7 @@ app.post("/api/notes", function(req, res) {
   });
 
   // Start our server
-server.listen(PORT, function () {
-    // Callback triggered when server is successfully listening. Hurray!
+app.listen(PORT, function () {
+        // Callback triggered when server is successfully listening. Hurray!
     console.log("App listening on PORT " + PORT);
 });
